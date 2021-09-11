487e5cfa916e35e9f4398559c63fca50f2e2804b
citywizz <stephane.arrami@gmail.com>
2021-09-11 20:39:42 +0200

    Exercice TypeScript organiser un projet

---
+++ final/dist/app.js
@@ -0,0 +1,3 @@
+"use strict";
+var app = document.querySelector("#app");
+app.innerHTML = "<h1>Exemple</h1><main>Coucou !</main>";
---
+++ final/src/app.ts
@@ -0,0 +1,4 @@
+const app = <HTMLDivElement>document.querySelector("#app");
+
+app.innerHTML="<h1>Exemple</h1><main>Coucou !</main>"
+
